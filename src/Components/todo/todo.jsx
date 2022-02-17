e9251db350b126fbb81b76e487370a789acb0d09
alizapeikes <79273849+alizapeikes@users.noreply.github.com>
2022-02-17 01:30:36 -0500

    Minor changes

--- src/Components/todo/todo.jsx
+++ src/Components/todo/todo.jsx
@@ -2,9 +2,6 @@ import './todo.css';
 import React, {useState} from 'react';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemText from '@mui/material/ListItemText';
-import ListItemAvatar from '@mui/material/ListItemAvatar';
 import Checkbox from '@mui/material/Checkbox';
 import TextField from '@mui/material/TextField';
 import Box from '@mui/material/Box';
@@ -59,8 +56,9 @@ const TodoItems = () =>{
                 onAdd={addTask}
             />
 
-            <Tasks list={list}
-                   setList={setList} />
+            <Tasks 
+                list={list}
+                setList={setList} />
         </div>
     )
 }
@@ -138,49 +136,3 @@ const Tasks = ({list,setList}) => {
     </div>
     );
 }
-
-/*
-const Tasks = ({list}) => {
-    const [checked, setChecked] = React.useState([1]);
-    const [style, setStyle] = useState("cont");
-
-    const handleToggle = (value) => () => {
-        const currentIndex = checked.indexOf(value);
-        const newChecked = [...checked];
-
-        if (currentIndex === -1) {
-            newChecked.push(value);
-        } else {
-            newChecked.splice(currentIndex, 1);
-        }
-        
-        setChecked(newChecked);
-
-    };
-
-
-    return(
-    <div>
-        <List>
-            {list.map((item) => (
-                
-                <ListItem className={style} primary={item.task}>{item.task}
-                <Checkbox
-                    edge = "end"
-                    onChange={handleToggle(item)}
-                    checked={checked.indexOf(item) !== -1}
-                    className="iconButton"
-                 ></Checkbox>
-                 
-                 <IconButton className="iconButton" edge="end" aria-label="delete">
-                 <DeleteOutlineOutlinedIcon />
-                </IconButton>
-                
-                 
-                </ListItem>
-            ))}
-        </List>
-    </div>
-    );
-}
-*/
